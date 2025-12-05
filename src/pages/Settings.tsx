f37344e421cab2efc086393262dac05cc1ec58a5
Jorens Merenjanu <jorensmerenjanu@gmail.com>
2025-12-06 00:57:12 +0200

    Refactor Settings component layout

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -31,28 +31,32 @@ export default function SettingsPage() {
 
     return (
         <div className='h-full w-full flex flex-col justify-between'>
-            <h2>Settings</h2>
-            <ul className='h-fit flex flex-col gap-2'>
-                <li className='flex gap-1'>
-                    <input 
-                        className='mt-0.5'
-                        type='checkbox' 
-                        checked={settings.retainMusic}
-                        onChange={(e) => updateSettings({retainMusic: e.currentTarget.checked})} 
-                    />
-                    <label>
-                        Keep music on reopen
-                    </label>
-                </li>
-                <li>
-                    <button onClick={toggleWakeLockEnabled}>Wake Lock {settings.wakeLock ? 'On' : 'Off'}</button>
-                </li>
-            </ul>
-            <ul className='h-fit flex flex-col gap-2'>
-                <li>
-                    <a href="https://checkout.revolut.com/pay/db94d317-bb0b-4f87-9506-854899b05981" target="_blank">Support Development</a>
-                </li>
-            </ul>
+            <div className='flex flex-col'>
+                <h2>Settings</h2>
+                <ul className='h-fit flex flex-col gap-2'>
+                    <li className='flex gap-1'>
+                        <input 
+                            className='mt-0.5'
+                            type='checkbox' 
+                            checked={settings.retainMusic}
+                            onChange={(e) => updateSettings({retainMusic: e.currentTarget.checked})} 
+                        />
+                        <label>
+                            Keep music on reopen
+                        </label>
+                    </li>
+                    <li>
+                        <button onClick={toggleWakeLockEnabled}>Wake Lock {settings.wakeLock ? 'On' : 'Off'}</button>
+                    </li>
+                </ul>
+            </div>
+            <div className='flex flex-col'>
+                <ul className='h-fit flex flex-col gap-2'>
+                    <li>
+                        <a href="https://checkout.revolut.com/pay/db94d317-bb0b-4f87-9506-854899b05981" target="_blank">Support Development</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
