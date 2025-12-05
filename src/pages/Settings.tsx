3b370f5bdd4a311a5ddd9358e5a1e0f1b7a7bd41
Jorens Merenjanu <jorensmerenjanu@gmail.com>
2025-12-06 00:54:05 +0200

    Update Settings component for accessibility and links

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -33,7 +33,7 @@ export default function SettingsPage() {
         <div className='h-full w-full flex flex-col justify-between'>
             <h2>Settings</h2>
             <ul className='h-fit flex flex-col gap-2'>
-                <li className='flex items-center gap-1'>
+                <li className='flex gap-1'>
                     <input 
                         className='mt-0.5'
                         type='checkbox' 
@@ -50,7 +50,7 @@ export default function SettingsPage() {
             </ul>
             <ul className='h-fit flex flex-col gap-2'>
                 <li>
-                    <a href="https://checkout.revolut.com/pay/db94d317-bb0b-4f87-9506-854899b05981">Support Development</a>
+                    <a href="https://checkout.revolut.com/pay/db94d317-bb0b-4f87-9506-854899b05981" target="_blank">Support Development</a>
                 </li>
             </ul>
         </div>
