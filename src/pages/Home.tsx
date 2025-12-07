b4c5f2aa4432cbcd2b996d5d1ee7c116345e58bf
Jorens Merenjanu <jorensmerenjanu@gmail.com>
2025-12-07 19:09:50 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,10 +8,12 @@ export default function HomePage() {
     return (
         <div>
             <main>
+                <h1>Get yourself in the mood for sleep</h1>
                 <p>
                     Time To Sleep is a small application that helps you get into the mood
                     for sleeping.
                 </p>
+<p>Listen to soothing sounds like rain or piano, do sleep affirmations or breathing exercises</p>
                 <Link
                     href={routes.app.url}
                 >
